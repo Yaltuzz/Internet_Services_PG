59ca953987096a7115b8800fcadaffe53b5b4414
Jan Stopinski <jan.stopinski@devideal.com>
2022-11-06 04:07:36 +0100

    add producer, configure consumer, add rabbitmq

--- Dotnet_Service/Internet_Services_PG/Program.cs
+++ Dotnet_Service/Internet_Services_PG/Program.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace Internet_Services_PG
 {
@@ -13,6 +16,25 @@ namespace Internet_Services_PG
     {
         public static void Main(string[] args)
         {
+            var factory = new ConnectionFactory() { HostName = "rabbit" };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.QueueDeclare(queue: "hello",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Received {0}", message);
+            };
+            channel.BasicConsume(queue: "hello",
+                autoAck: true,
+                consumer: consumer);
             CreateHostBuilder(args).Build().Run();
         }
 
